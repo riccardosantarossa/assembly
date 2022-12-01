5f19fa3875341d6015c71ccb810fc68b767f84aa
Riccardo Santarossa <brudo1968@libero.it>
2022-12-01 12:12:07 +0100

    sostituisce a 0 i primi 4 numeri naturali

--- es2.1 assembly.s
+++ es2.1 assembly.s
@@ -8,13 +8,13 @@ vettore: .word 0,0,0,0
 
 ldr r3, =vettore
 mov r0, #1   @carico in r0 il numero 1
-str r3, [r0] @sposto nella prima cella del vettore, e così via
+str r0, [r3] @sposto nella prima cella del vettore, e così via
 mov r0, #2
-str r3, [r0, #4]
+str r0, [r3, #4]
 mov r0, #3
-str r3, [r0, #8]
+str r0, [r3, #8]
 mov r0, #4
-str r3, [r0, #12]
+str r0, [r3, #12]
 
 .end
 
